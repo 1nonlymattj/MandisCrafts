c3663fcd69efd969c2df237113b4351c5fc911ed
Matthew Johnson <matthew.johnson.185.ctr@us.af.mil>
2020-11-06 12:31:55 -0600

    working on updated email script

--- WebContent/js/cart.js
+++ WebContent/js/cart.js
@@ -200,6 +200,83 @@ $('.show-cart').on("change", ".item-count", function(event) {
 
 displayCart();
 
+var placeSearch, autocomplete;
+
+//List all address components (corresponds to form field IDs and Google address object)
+var componentForm = {
+autocomplete: ['street_number', 'route'],
+inputCity: 'locality',
+inputState: 'administrative_area_level_1',
+inputZip: 'postal_code',
+inputCounty: 'administrative_area_level_2',
+inputCountry: 'country'
+};
+
+//Create autocomplete object based on the autocomplete ("street") field
+//Location type restricted to geocode
+function initAutocomplete() {
+autocomplete = new google.maps.places.Autocomplete(
+   /** @type {!HTMLInputElement} */ (document.getElementById('autocomplete')),
+   {type: ['geocode']});
+
+ // Call fillInAddress when user selects an address from dropdown
+autocomplete.addListener('place_changed', fillInAddress);
+}
+
+//Fill fields with values from Google Maps autocomplete object
+function fillInAddress() {
+
+// Get place data from autocomplete object
+var place = autocomplete.getPlace();
+console.log(place);
+
+// Enable each field, then fill them with the corresponding value from the place object
+for (var component in componentForm) {
+ document.getElementById(component).disabled = false;
+ document.getElementById(component).value = search(componentForm[component], place.address_components);
+}
+
+ // Original Google Implementation - do not use
+// Get each component of the address from the place
+// object and fill the corresponding field
+//for (var i = 0; i < place.address_components.length; i++) {
+
+//  var addressType = place.address_components[i].types[0];
+
+//  if (componentForm[addressType]) {
+//    var val = place.address_components[i][componentForm[addressType]];
+//    document.getElementById(addressType).value = val;
+//  }
+//}
+
+// Fill the autocomplete field with values from the place object
+// If a street number is not found, set the field to route only.
+if (search("street_number", place.address_components) != "") {
+ document.getElementById("autocomplete").value = search("street_number", place.address_components) + " ";
+}
+document.getElementById("autocomplete").value += search("route", place.address_components);
+
+// Search the passed object for a specified address component/type and return the short_name value of the matched component/type
+// If requested type does not exist in the placeObject, return an empty string
+function search(type, placeObject) {
+ for (var i = 0; i < placeObject.length; i++) {
+   if (placeObject[i].types[0] === type) {
+     return placeObject[i].short_name;
+   } else if (i === placeObject.length - 1) {
+     return "";
+   }
+ }
+}
+}
+
+$('#paymentPref').change(function () {
+    if($(this).is(':checked')) {
+        $('#paymentPref').attr('required');
+    } else {
+        $('#paymentPref').removeAttr('required');
+    }
+});
+
 function sendMail() {
    var body = JSON.stringify(sessionStorage.getItem('shoppingCart'), null, '&nbsp').split(',').join('%0D%0A');
    var bodyPretty = JSON.parse(body);
@@ -211,9 +288,16 @@ function sendMail() {
    var items = "Items Purchased: ";
    var totalDue = "Total Amount Due When Item(s) Are Complete: "
    var total = shoppingCart.totalCart();
+   var payment = item.value;
+   //var bodyString = info + '%0D%0A' + customer + '%0D%0A' + phone + '%0D%0A' + address + '%0D%0A %0D%0A';
+   var bodyString = 'Please Fill Out Information Below: %0D%0A Name: %0D%0A Phone: %0D%0A Address: %0D%0A %0D%0A'
    window.open('mailto:mattj5609@gmail.com?cc=mandimay5609@gmail.com&subject=Mandis Craft Boutique Order Form&body='
-         + notice + '%0D%0A %0D%0A' + info + '%0D%0A' + customer + '%0D%0A' + phone + '%0D%0A' + address + '%0D%0A %0D%0A' + items + '%0D%0A' + bodyPretty + '%0D%0A %0D%0A' + totalDue + total);
+         + notice + '%0D%0A %0D%0A' + bodyString 
+         + 'preferred payment type: %0D%0A' + payment + 'Venmo Name if selected: ' + '%0D%0A %0D%0A'
+         + items + '%0D%0A' + bodyPretty + '%0D%0A %0D%0A' + totalDue + total);
 }
+
+
 
 //Array to store our Snowflake objects
 var snowflakes = [];
